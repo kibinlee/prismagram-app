f4fea02699a83a8107ee8f352f7c80388aa465b4
KBL <kibinlee19k@gmail.com>
2020-02-12 23:45:33 +0900

    Tabs Navigation

--- components/NavController.js
+++ components/NavController.js
@@ -2,21 +2,17 @@ import React from "react";
 import { Text, View, TouchableOpacity } from "react-native";
 import { useIsLoggedIn, useLogIn, useLogOut } from "../AuthContext";
 import AuthNavigation from "../navigation/AuthNavigation";
+import TabNavigation from "../navigation/TabNavigation";
 
 export default () => {
-  const isLoggedIn = useIsLoggedIn();
+  // const isLoggedIn = useIsLoggedIn();
+  const isLoggedIn = true;
   // console.log(isLoggedIn);
-  const logIn = useLogIn();
-  const logOut = useLogOut();
+  // const logIn = useLogIn();
+  // const logOut = useLogOut();
   return (
     <View style={{ flex: "1" }}>
-      {isLoggedIn ? (
-        <TouchableOpacity onPress={logOut}>
-          <Text>Log Out</Text>
-        </TouchableOpacity>
-      ) : (
-        <AuthNavigation />
-      )}
+      {isLoggedIn ? <TabNavigation /> : <AuthNavigation />}
     </View>
   );
 };
