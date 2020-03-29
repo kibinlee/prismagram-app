37cdaf4b21d69c3c4095631a0dea72e630da9004
KBL <kibinlee19k@gmail.com>
2020-03-29 13:58:50 +0900

    Login part Three

--- screens/Auth/Login.js
+++ screens/Auth/Login.js
@@ -18,7 +18,7 @@ const View = styled.View`
 export default ({ navigation }) => {
   const emailInput = useInput("");
   const [loading, setLoading] = useState(false);
-  const requestSecret = useMutation(LOG_IN, {
+  const [requestSecretMutation] = useMutation(LOG_IN, {
     variables: {
       email: emailInput.value
     }
@@ -35,9 +35,18 @@ export default ({ navigation }) => {
     }
     try {
       setLoading(true);
-      await requestSecret();
-      Alert.alert("Check your email");
-      navigation.navigate("Confirm");
+      const {
+        data: { requestSecret }
+      } = await requestSecretMutation();
+      // await requestSecret();
+      if (requestSecret) {
+        Alert.alert("Check your email");
+        navigation.navigate("Confirm");
+        return;
+      } else {
+        Alert.alert("Account not found");
+        navigation.navigate("Signup");
+      }
     } catch (e) {
       Alert.alert("Can't log in now");
     } finally {
@@ -52,7 +61,7 @@ export default ({ navigation }) => {
           placeholder="Email"
           keyboardType="email-address"
           returnKeyType="send"
-          onEndEditing={handleLogin}
+          onSubmitEditing={handleLogin}
           autoCorrect={false}
         />
         <AuthButton loading={loading} onPress={handleLogin} text="Log In" />
